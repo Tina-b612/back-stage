2cb8991f0433606c74821d3034e03e75a993d71e
Tina-b612 <tina__1995@163.com>
2017-03-23 15:10:05 +0800

    搭建路由结结构

---
+++ src/main.js
@@ -0,0 +1,44 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+
+// element-ui
+import ElementUI from 'element-ui'
+import '../theme/index.css'
+Vue.use(ElementUI)
+
+//vue-resource
+import vueResource from 'vue-resource'
+Vue.use(vueResource);
+
+// vuex
+import Vuex from 'vuex';
+Vue.use(Vuex);
+
+//上传图片
+//import VueCoreImageUpload  from 'vue.core.image.upload';
+
+//reset CSS
+import "./assets/css/reset.css"
+import "./assets/css/main.css"
+
+
+// root component 
+import App from './App'
+import router from './router'
+import store from './vuex/store'
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  store,
+  template: '<App/>',
+  components: { 
+  	App
+  },
+  beforeCreate:function (){
+	router.push({path:'/'})
+  }
+})
